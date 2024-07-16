7f5d2f2c1e645f16233fb0fae8c132e9983dab33
surya pelluru <maladikrishnakishore@gmail.com>
2024-07-16 23:49:20 +0530

    Infra

---
+++ 50-acm/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "ingress-alb"
+  }
+}
+
+variable "zone_name" {
+  default = "devopskk.online"
+}
+
+variable "zone_id" {
+  default = "Z100663530MNTX07G8NYH"
+}
